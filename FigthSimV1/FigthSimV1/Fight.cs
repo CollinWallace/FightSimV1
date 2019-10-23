45eedaf298a71ab4652ff8e8a33b142d82c3edf6
CollinWallace <54947130+CollinWallace@users.noreply.github.com>
2019-10-23 14:25:42 +0200

    Klar

---
+++ FigthSimV1/FigthSimV1/Fight.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigthSimV1
+{
+    class Fighter
+    {
+        private int hp = 100;
+        public string name;
+        public int MINdmg = 5;
+        public int MAXdmg = 15;
+
+        static Random Genrator = new Random();//statc gör den univrsal
+
+        public int Attack()
+        {
+            int Damage = Genrator.Next(MINdmg, MAXdmg);
+            return Damage;
+        }
+
+        public void Hurt(int Damage)
+        {
+            hp -= Damage;
+        }
+
+        public bool IsAlive()
+        {
+           return hp > 0;
+        }
+
+        public int GetHP()
+        {
+            return hp;
+        }
+        
+
+    }
+}
---
+++ FigthSimV1/FigthSimV1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigthSimV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Fighter A = new Fighter();
+           Fighter B = new Fighter();
+            int turn = 1;
+
+            Console.WriteLine("WRITE A NAME FOR FIGHTER A:");
+            A.name = Console.ReadLine();
+            Console.WriteLine("Fighter A is:" +A.name);
+            //Console.ReadLine();
+
+            Console.WriteLine("WRITE A NAME FOR FIGHTER b:");
+            B.name = Console.ReadLine();
+            Console.WriteLine("Fighter B is:" + B.name);
+            Console.ReadLine();
+
+            while(A.IsAlive() && B.IsAlive())
+            {
+                Console.WriteLine("TURN: "+turn);
+                A.Hurt(B.Attack());
+                Console.WriteLine(A.name + " has "+A.GetHP()+"HP left");
+
+                B.Hurt(A.Attack());
+                Console.WriteLine(B.name + " has " + B.GetHP() + "HP left");
+                Console.ReadLine();
+
+                turn++;
+            }
+
+            if(A.IsAlive()==false || B.IsAlive() ==true )
+            {
+                Console.WriteLine("Fighter: " + B.name + " has won!");
+                Console.ReadLine();
+            }
+            if (B.IsAlive() == false || A.IsAlive() == true)
+            {
+                Console.WriteLine("Fighter: " + A.name + " has won!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
