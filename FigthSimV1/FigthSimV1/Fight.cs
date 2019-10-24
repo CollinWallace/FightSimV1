3c1486dccbd3ecc7dc1dc3b26d35a9fb553ca778
CollinWallace <54947130+CollinWallace@users.noreply.github.com>
2019-10-24 21:56:47 +0200

    Skrev kommentarer

--- FigthSimV1/FigthSimV1/Fight.cs
+++ FigthSimV1/FigthSimV1/Fight.cs
@@ -8,30 +8,30 @@ namespace FigthSimV1
 {
     class Fighter
     {
-        private int hp = 100;
-        public string name;
-        public int MINdmg = 5;
-        public int MAXdmg = 15;
+        private int hp = 100; // Skapar en private int  för hp vilket innebär att den endast kan användas inom denna class.
+        public string name; //Skapar en public string för name vilket innebär att den kan användas utanför metoden.
+        public int MINdmg = 5; //Int för mininum damage roll
+        public int MAXdmg = 15; //int för maximum damage roll
 
-        static Random Genrator = new Random();//statc gör den univrsal
+        static Random Genrator = new Random();//Skapar en random generator, static gör den universal, så att alla ändringar som görs här updateras i alla andra instanser a metoden
 
         public int Attack()
         {
-            int Damage = Genrator.Next(MINdmg, MAXdmg);
-            return Damage;
+            int Damage = Genrator.Next(MINdmg, MAXdmg); //int damage genererar ett värde mellan MINdmg och MAXdmg.
+            return Damage; //Detta returnerar värdet som genereras så att den kan användas utanför.
         }
 
         public void Hurt(int Damage)
         {
-            hp -= Damage;
+            hp -= Damage; //Här tar man värdet av hp minus värdet som genereras i damage.
         }
 
-        public bool IsAlive()
+        public bool IsAlive() //Detta kollar så att så länge hp är över 0 så är boolen = True, vilket innebär att spelet fortsätter
         {
            return hp > 0;
         }
 
-        public int GetHP()
+        public int GetHP() //Denna metod är helt enkelt bara till för att kunna visa vilket värde inten HP har just nu.
         {
             return hp;
         }
--- FigthSimV1/FigthSimV1/Program.cs
+++ FigthSimV1/FigthSimV1/Program.cs
@@ -10,39 +10,39 @@ namespace FigthSimV1
     {
         static void Main(string[] args)
         {
-           Fighter A = new Fighter();
-           Fighter B = new Fighter();
-            int turn = 1;
+           Fighter A = new Fighter(); //Med detta så kallar jag in metoden Fighter in i Main för spelaren A
+           Fighter B = new Fighter(); //Skapar spelaren B med metoden fighter.
+           int turn = 1; //Skapar ett start värde för turn
 
             Console.WriteLine("WRITE A NAME FOR FIGHTER A:");
-            A.name = Console.ReadLine();
-            Console.WriteLine("Fighter A is:" +A.name);
-            //Console.ReadLine();
+            A.name = Console.ReadLine(); // Med detta så lägger jag till stringen "name" till "A" och säger att det är lika med console.readline så att användaren kan lägga in sitt egna input och det inputet lagras i "A".
+            Console.WriteLine("Fighter A is:" +A.name); //Skriver ut namnet på A som användaren skrivit in.
+            
 
             Console.WriteLine("WRITE A NAME FOR FIGHTER b:");
-            B.name = Console.ReadLine();
+            B.name = Console.ReadLine(); //Det som nu skrivs in i readline lagras istället i stringen jag kopplat till B
             Console.WriteLine("Fighter B is:" + B.name);
             Console.ReadLine();
 
-            while(A.IsAlive() && B.IsAlive())
+            while(A.IsAlive() && B.IsAlive()) //Så länge A.IsAlive och B.IsAlive är true (Alltså att hp är över 0) så fortsätter denna loop.
             {
-                Console.WriteLine("TURN: "+turn);
-                A.Hurt(B.Attack());
-                Console.WriteLine(A.name + " has "+A.GetHP()+"HP left");
+                Console.WriteLine("TURN: "+turn); //Visar int turn
+                A.Hurt(B.Attack()); // Detta gör så att spelaren B får en slumpmässig "B.Attack" som sedan med hjälp av A.Hurt subtraherar "B.Attack" med Spelaren As HP.
+                Console.WriteLine(A.name + " has "+A.GetHP()+"HP left"); //GetHP returnerar då det värde som inten HP har kvar och visar den i konsollen.
 
-                B.Hurt(A.Attack());
+                B.Hurt(A.Attack()); //Samma som ovan fast med spelaren A
                 Console.WriteLine(B.name + " has " + B.GetHP() + "HP left");
                 Console.ReadLine();
 
-                turn++;
+                turn++; //Varje gång loopen körs adderas inten med 1
             }
 
-            if(A.IsAlive()==false || B.IsAlive() ==true )
+            if(A.IsAlive()==false || B.IsAlive() ==true ) //Så länge A.IsAlive är false (Död) och B.IsAlive är true (lever) så vinner spelaren B, då det innebär att spelaren A har ett HP (int) som är under/lika med 0.
             {
                 Console.WriteLine("Fighter: " + B.name + " has won!");
                 Console.ReadLine();
             }
-            if (B.IsAlive() == false || A.IsAlive() == true)
+            if (B.IsAlive() == false || A.IsAlive() == true) //Samma som ovan fast tvärtom.
             {
                 Console.WriteLine("Fighter: " + A.name + " has won!");
                 Console.ReadLine();
